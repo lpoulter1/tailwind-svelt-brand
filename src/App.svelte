54453c8484331f04eebb6d7f343c04004961776f
lpoultersky <lpoultersky@gmail.com>
2023-04-04 09:21:27 +0100

    set up and and brand color

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import "./app.css";
+  import Brand from "./lib/Brand.svelte";
+</script>
+
+<main class="container px-4 py-8 mx-auto">
+  <h1 class="text-3xl font-bold mb-3">Brand Guidlines</h1>
+
+  <Brand />
+</main>
---
+++ src/lib/Brand.svelte
@@ -0,0 +1,45 @@
+<section class="h-screen">
+  <h2 class="text-2xl font-semibold mb-3">Brand Colors</h2>
+  <div class="py-2 mb-6">
+    <hr class="border-b border-[#CCCCCC] border-dashed" />
+  </div>
+  <div>
+    <h3 class="text-xl mb-6">Main Color</h3>
+    <p class="mb-6">
+      These bold colors add energy to our brand, while highlighting the
+      simplicity of our designs.
+    </p>
+  </div>
+  <div class="flex gap-2 justify-between">
+    <div class="flex justify-center items-center flex-col">
+      <div class="rounded-full mb-3 bg-[#231F20] h-[90px] w-[90px] shadow-md" />
+      <div class="mb-3">True Black</div>
+      <div>#231F20</div>
+      <div>35, 31, 32</div>
+    </div>
+    <div class="flex justify-center items-center flex-col">
+      <div class="rounded-full mb-3 bg-[#58595B] h-[90px] w-[90px] shadow-md" />
+      <div class="mb-3">True Gray</div>
+      <div>#58595B</div>
+      <div>88, 89, 91</div>
+    </div>
+    <div class="flex justify-center items-center flex-col">
+      <div class="rounded-full mb-3 bg-[#FED107] h-[90px] w-[90px] shadow-md" />
+      <div class="mb-3">Dandelion</div>
+      <div>#FED107</div>
+      <div>254, 209, 7</div>
+    </div>
+    <div class="flex justify-center items-center flex-col">
+      <div class="rounded-full mb-3 bg-[#EF3F40] h-[90px] w-[90px] shadow-md" />
+      <div class="mb-3">Radiant Red</div>
+      <div>#EF3F40</div>
+      <div>239, 63, 64</div>
+    </div>
+    <div class="flex justify-center items-center flex-col">
+      <div class="rounded-full mb-3 bg-[#FFFFFF] h-[90px] w-[90px] shadow-md" />
+      <div class="mb-3">Pure White</div>
+      <div>#FFFFFF</div>
+      <div>255, 255, 255</div>
+    </div>
+  </div>
+</section>
