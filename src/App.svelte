ddc04e6b0a12331979ed5e4dbf302866a84fc612
lpoultersky <lpoultersky@gmail.com>
2023-04-04 09:54:35 +0100

    implement scroll snap

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,9 @@
   import Brand from "./lib/Brand.svelte";
 </script>
 
-<main class="container px-4 py-8 mx-auto">
-  <h1 class="text-3xl font-bold mb-3">Brand Guidlines</h1>
-
-  <Brand />
+<main class="container mx-auto">
+  <!-- <h1 class="text-3xl font-bold mb-3">Brand Guidlines</h1> -->
+  <div class="snap-y snap-mandatory h-screen overflow-x-hidden">
+    <Brand />
+  </div>
 </main>
